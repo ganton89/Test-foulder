e9f28828740cf5bc3d39546a710b2b5967ad9be1
gema Anton <ganto@lesmed24.medlumics.com>
2025-07-23 15:53:05 +0200

    ok

--- cadenas.py
+++ cadenas.py
@@ -3,3 +3,14 @@ lastname = "anton"
 print(f"{name} {lastname}")
 print(name.upper())
 print(lastname.lower()) 
+
+print(name.capitalize())
+a= 10
+b= 5    
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("Potenciación:", a ** b)
+print("División:", a / b)
+print("Parte entera de la división:", a // b)
+print("Módulo:", a % b)
