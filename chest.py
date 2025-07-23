aec89c3b35e26179b214966b2ede6df68c845fde
gema Anton <ganto@lesmed24.medlumics.com>
2025-07-23 17:12:22 +0200

    ok

---
+++ chest.py
@@ -0,0 +1,13 @@
+chess_board = [
+    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    ['R', 'N', 'B', 'Q', 'Q', 'K', 'B', 'N', 'R']
+]
+
+print(chess_board[2][2])  # Print the piece at the top-left corner
+print(type(chess_board))    
