bdd3d8eb805b8416adaab63e88c9631a78cf9d29
gema Anton <ganto@lesmed24.medlumics.com>
2025-07-23 16:12:26 +0200

    ok

--- cadenas.py
+++ cadenas.py
@@ -1,16 +1,21 @@
-name = "gema"
-lastname = "anton"
-print(f"{name} {lastname}")
-print(name.upper())
-print(lastname.lower()) 
+#name = "gema"
+#lastname = "anton"
+#print(f"{name} {lastname}")
+#print(name.upper())
+#print(lastname.lower()) 
 
-print(name.capitalize())
-a= 10
-b= 5    
-print("Suma:", a + b)
-print("Resta:", a - b)
-print("Multiplicación:", a * b)
-print("Potenciación:", a ** b)
-print("División:", a / b)
-print("Parte entera de la división:", a // b)
-print("Módulo:", a % b)
+#print(name.capitalize())
+#a= 10
+#b= 5    
+#print("Suma:", a + b)
+#print("Resta:", a - b)
+#print("Multiplicación:", a * b)
+#print("Potenciación:", a ** b)
+#print("División:", a / b)
+#print("Parte entera de la división:", a // b)
+#print("Módulo:", a % b)
+
+name = input("Ingrese su nombre: ")
+lastname = input("Ingrese su apellido: ")
+print("Nombre:", name.upper)
+print("Apellido:", lastname.lower())  
