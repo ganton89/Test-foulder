844ebdd4f24f7fa3c548d3a52f25dc30330db390
gema Anton <ganto@lesmed24.medlumics.com>
2025-07-24 13:04:37 +0200

    Practica de condicionales

---
+++ Condicional.py
@@ -0,0 +1,16 @@
+x = int(input("Enter a number: "))
+
+if x > 5:
+    print("x is greater than 5") 
+elif x == 5:
+    print("x is equal to 5")
+else:
+    print("x is not greater than 5")
+
+
+y = 10
+
+if y > 5 and x < 15:
+    print("Se cumple la condicion y esta entre 5 y 15")
+else:
+    print("No se cumple")
