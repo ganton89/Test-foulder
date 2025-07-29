a9d9306dd3762d14512ed65e2046a09befba96fd
gema Anton <ganto@lesmed24.medlumics.com>
2025-07-29 20:09:16 +0200

    ejemplo de herencia

--- funciones.py
+++ funciones.py
@@ -1,5 +1,15 @@
-def greet (name =input("¿Cuál es tu nombre? ")):
+def greet (name):
+  name = input("Ingrese su nombre: ")
   print("Hola", name,"!")
 
-greet()     
+
+def sumatorio(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n + sumatorio(n - 1)
+
+print(sumatorio(5))  # Imprime 15, que es 5 + 4 + 3 + 2 + 1
+
+
 
