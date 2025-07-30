2e5f9620e22ec9f4e75246a8cc0983a6a8eed593
gema Anton <ganto@lesmed24.medlumics.com>
2025-07-30 09:49:15 +0200

    Textos

--- open-caperucita.py
+++ open-caperucita.py
@@ -1,4 +1,5 @@
-with open("caperucita.txt", "a") as file:
-    file.write("Caperucita Roja es un personaje de cuento de hadas que se caracteriza por su capa roja y su viaje a casa de su abuela. En el camino, se encuentra con un lobo que intenta engañarla. La historia enseña lecciones sobre la obediencia y la precaución.\n")    
-    
+with open("caperucita.txt", "r") as file:
+    content = file.read().strip()
+    lines = content.count() + 1
+    print(lines)    
 
